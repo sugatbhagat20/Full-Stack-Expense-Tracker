d1a04ef3d14c74a84242182fb8e20fa71f1f179c
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-12-05 14:55:13 +0530

    Making Expense App Production ready

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,69 +1,75 @@
 const expenses = require("../model/expenseModel");
 const users = require("../model/userModel");
 const sequelize = require("../utils/database");
-exports.addExpense = (req, res, next) => {
-  console.log(req.body);
-  const name = req.body.name;
-  const amount = req.body.amount;
-  const expense = req.body.expense;
 
-  const totalExpense = Number(req.user.totalExpenses) + Number(amount);
-  users
-    .update(
+exports.addExpense = async (req, res, next) => {
+  console.log(req.body);
+  const t = await sequelize.transaction();
+  try {
+    const name = req.body.name;
+    const amount = req.body.amount;
+    const expense = req.body.expense;
+
+    const totalExpense = Number(req.user.totalExpenses) + Number(amount);
+    await users
+      .update(
+        {
+          totalExpenses: totalExpense,
+        },
+        { where: { id: req.user.id } },
+        { transaction: t }
+      )
+      .catch((err) => console.error(err));
+
+    await expenses
+      .create(
+        {
+          name: name,
+          amount: amount,
+          expense: expense,
+          userId: req.user.id,
+        },
+        { transaction: t }
+      )
+      .then((result) => {
+        console.log("Added to Expense");
+        //res.redirect("/get");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    await t.commit();
+  } catch {
+    async (err) => {
+      await t.rollback();
+      console.log(err);
+    };
+  }
+};
+
+exports.getExpenses = async (req, res, next) => {
+  try {
+    const expense = await expenses.findAll({ where: { userId: req.user.id } });
+    await res.json(expense);
+  } catch (err) {
+    console.log(err);
+  }
+};
+//
+
+exports.deleteExpense = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const expense = await expenses.findByPk(id);
+    await users.update(
       {
-        totalExpenses: totalExpense,
+        totalExpenses: req.user.totalExpenses - expense.amount,
       },
       { where: { id: req.user.id } }
-    )
-    .catch((err) => console.error(err));
-
-  expenses
-    .create({
-      name: name,
-      amount: amount,
-      expense: expense,
-      userId: req.user.id,
-    })
-    .then((result) => {
-      console.log("Added to Expense");
-      //res.redirect("/get");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-exports.getExpenses = (req, res, next) => {
-  expenses
-    .findAll({ where: { userId: req.user.id } })
-    .then((expenses) => {
-      res.json(expenses);
-      // console.log(users);
-    })
-    .catch((err) => console.log(err));
-};
-
-exports.deleteExpense = (req, res, next) => {
-  const id = req.params.id;
-  expenses
-    .findByPk(id)
-    .then((expense) => {
-      totalExpense = Number(req.user.totalExpenses) - Number(expense.amount);
-      users
-        .update(
-          {
-            totalExpenses: totalExpense,
-          },
-          { where: { id: req.user.id } }
-        )
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
-
-  expenses
-    .destroy({ where: { id: id, userId: req.user.id } })
-    .then((result) => {
-      res.redirect("/expense");
-    })
-    .catch((err) => console.log(err));
+    );
+    await expenses.destroy({ where: { id: id, userId: req.user.id } });
+    res.redirect("/expense");
+  } catch (err) {
+    console.log(err);
+  }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,84 +9,100 @@ function generateAccessToken(id, email) {
   );
 }
 
-exports.isPremiumUser = (req, res, next) => {
-  if (req.user.isPremiumUser) {
-    return res.json({ isPremiumUser: true });
+exports.isPremiumUser = async (req, res, next) => {
+  try {
+    if (req.user.isPremiumUser) {
+      return res.json({ isPremiumUser: true });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
-exports.addUser = (req, res, next) => {
+exports.addUser = async (req, res, next) => {
   console.log(req.body);
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  bcrypt.hash(password, 10, async (err, hash) => {
-    await users
-      .create({
-        name: name,
-        email: email,
-        password: hash,
+    bcrypt.hash(password, 10, async (err, hash) => {
+      await users
+        .create({
+          name: name,
+          email: email,
+          password: hash,
+        })
+        .then((result) => {
+          console.log("User Added");
+          res.status(201).json(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.logIn = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    users
+      .findOne({ where: { email: email } })
+      .then((user) => {
+        if (user) {
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (result === true) {
+              return res.status(200).json({
+                success: true,
+                message: "Login Successful!",
+                token: generateAccessToken(user.id, user.email),
+              });
+            } else {
+              return res
+                .status(401)
+                .json({ success: false, message: "Password Incorrect!" });
+            }
+          });
+        } else {
+          return res
+            .status(404)
+            .json({ success: false, message: "User doesn't Exists!" });
+        }
       })
-      .then((result) => {
-        console.log("User Added");
-        res.status(201).json(result);
+      .catch(res);
+    {
+      console.log(res);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    users
+      .findAll({
+        attributes: [
+          [sequelize.col("name"), "name"],
+          [sequelize.col("totalExpenses"), "totalExpenses"],
+        ],
+        order: [[sequelize.col("totalExpenses"), "DESC"]],
+      })
+      .then((users) => {
+        const result = users.map((user) => ({
+          name: user.getDataValue("name"),
+          totalExpenses: user.getDataValue("totalExpenses"),
+        }));
+        res.send(JSON.stringify(result));
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
       });
-  });
-};
-
-exports.logIn = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  users
-    .findOne({ where: { email: email } })
-    .then((user) => {
-      if (user) {
-        bcrypt.compare(password, user.password, (err, result) => {
-          if (result === true) {
-            return res.status(200).json({
-              success: true,
-              message: "Login Successful!",
-              token: generateAccessToken(user.id, user.email),
-            });
-          } else {
-            return res
-              .status(401)
-              .json({ success: false, message: "Password Incorrect!" });
-          }
-        });
-      } else {
-        return res
-          .status(404)
-          .json({ success: false, message: "User doesn't Exists!" });
-      }
-    })
-    .catch(res);
-  {
-    console.log(res);
+  } catch (error) {
+    console.log(error);
   }
-};
-
-exports.getAllUsers = (req, res, next) => {
-  users
-    .findAll({
-      attributes: [
-        [sequelize.col("name"), "name"],
-        [sequelize.col("totalExpenses"), "totalExpenses"],
-      ],
-      order: [[sequelize.col("totalExpenses"), "DESC"]],
-    })
-    .then((users) => {
-      const result = users.map((user) => ({
-        name: user.getDataValue("name"),
-        totalExpenses: user.getDataValue("totalExpenses"),
-      }));
-      res.send(JSON.stringify(result));
-    })
-    .catch((err) => {
-      console.error(err);
-    });
 };
